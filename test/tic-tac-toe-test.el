9c07ade2552e8f44e562eddbd222a1783d1f1cdd
accidentalrebel <accidentalrebel@gmail.com>
2017-04-08 16:57:09 +0800

    Added tests to new functions.

--- test/tic-tac-toe-test.el
+++ test/tic-tac-toe-test.el
@@ -31,3 +31,21 @@
    (should
     (equal
      (buffer-string) "axx\nxbx\nxxc"))))
+
+(ert-deftest tic-tac-toe--switch-to-next-player ()
+  (with-temp-buffer
+    (setq-local tic-tac-toe--current-player-number 1)
+    (should (equal (tic-tac-toe--switch-to-next-player) 2))
+    (should (equal (tic-tac-toe--switch-to-next-player) 1))
+    (should (equal (tic-tac-toe--switch-to-next-player) 2))
+    (setq-local tic-tac-toe--current-player-number 3)
+    (should (equal (tic-tac-toe--switch-to-next-player) 1))
+    (setq-local tic-tac-toe--current-player-number 0)
+    (should (equal (tic-tac-toe--switch-to-next-player) 1))))
+
+(ert-deftest tic-tac-toe--get-current-symbol ()
+  (with-temp-buffer
+    (setq-local tic-tac-toe--current-player-number 1)
+    (should (equal (tic-tac-toe--get-current-symbol) "x"))
+    (setq-local tic-tac-toe--current-player-number 2)
+    (should (equal (tic-tac-toe--get-current-symbol) "o"))))
